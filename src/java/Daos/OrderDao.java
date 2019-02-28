563d5b9b95ac613fe83ea0269b67895063e5ac7a
D00188619 <D00188619@P1111-PC19.ad.dkit.ie>
2019-02-28 10:34:29 +0000

    Anime Dao etc. Anime genres from menu

---
+++ src/java/Daos/OrderDao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Daos;
+
+/**
+ *
+ * @author D00188619
+ */
+public class OrderDao extends Dao implements OrderDaoInterface {
+     public OrderDao(String databaseName) {
+        super(databaseName);
+    }
+}
